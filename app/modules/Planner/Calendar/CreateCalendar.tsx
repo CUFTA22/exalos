bdb85a2afb4c4f124adf640b841033e85530db74
Nikola <nikolajovanovic5556@gmail.com>
2021-08-16 02:35:37 +0200

    yeet

--- app/modules/Planner/Calendar/CreateCalendar.tsx
+++ app/modules/Planner/Calendar/CreateCalendar.tsx
@@ -1,8 +1,14 @@
+import { EyeShow24Regular } from '@fluentui/react-icons';
 import Card from '@lib/Card/Card';
+import FAB from '@lib/FAB/FAB';
 import styles from './Calendar.module.scss';
 
 const CreateCalendar = () => {
-  return <Card className={styles.calendar}></Card>;
+  return (
+    <Card className={styles.calendar}>
+      <FAB Icon={EyeShow24Regular} />
+    </Card>
+  );
 };
 
 export default CreateCalendar;
