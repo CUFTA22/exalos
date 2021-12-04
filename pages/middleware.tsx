f3856a408af3e3220c1617832bd6ad570cfe495c
Nikola <nikolajovanovic5556@gmail.com>
2021-12-04 20:40:35 +0100

    yeet

--- pages/middleware.tsx
+++ pages/middleware.tsx
@@ -1,6 +1,8 @@
 // pages/_middleware.ts
 import type { NextFetchEvent, NextRequest } from 'next/server';
 
-const Middleware = (req: NextRequest, ev: NextFetchEvent) => {};
+const Middleware = (req: NextRequest, ev: NextFetchEvent) => {
+  return <div>Middleware</div>;
+};
 
 export default Middleware;
