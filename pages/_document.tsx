d78fbbc03468fb6d263ac602c028546d2ffc5774
Nikola Jovanovic <nikolajovanovic5556@gmail.com>
2022-04-23 01:14:06 +0200

    yeet

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,21 +6,22 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icons/512x512.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/icons/512x512.svg" type="image/png" />
+          <meta name="background-color" content="#2a2a41" />
           <meta name="theme-color" content="#91cbf8" />
 
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:url" content="https://exalos.vercel.app" />
           <meta name="twitter:title" content="Exalos" />
           <meta name="twitter:description" content="App Description" />
-          <meta name="twitter:image" content="https://exalos.vercel.app/icons/192x192.svg" />
+          <meta name="twitter:image" content="https://exalos.vercel.app/icons/192x192.png" />
           <meta name="twitter:creator" content="@twitter" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Exalos" />
           <meta property="og:description" content="App Description" />
           <meta property="og:site_name" content="Exalos" />
           <meta property="og:url" content="https://exalos.vercel.app" />
-          <meta property="og:image" content="https://exalos.vercel.app/icons/192x192.svg" />
+          <meta property="og:image" content="https://exalos.vercel.app/icons/192x192.png" />
         </Head>
 
         <body>
