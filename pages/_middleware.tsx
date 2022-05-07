d7cead5780598988b0f01092e49c0c2cb4a5a693
Nikola Jovanovic <nikolajovanovic5556@gmail.com>
2022-05-07 21:39:16 +0200

    yeet

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -1,7 +1,10 @@
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse, NextRequest, NextFetchEvent } from 'next/server';
 
-export async function middleware(req, ev) {
-  const { pathname } = req.nextUrl;
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  // prettier-ignore
+  const { nextUrl: { pathname }, ip } = req;
+
+  return NextResponse.json(req.ip);
 
   if (pathname.includes('dashboard')) {
     return NextResponse.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
